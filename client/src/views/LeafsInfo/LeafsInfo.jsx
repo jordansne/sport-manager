f9debbc2c7367e619b88214dd18c0dd561d6858f
Jordan <jordansne@gmail.com>
2017-11-30 22:14:34 -0500

    Implement base LeafsInfo view

--- client/src/views/LeafsInfo/LeafsInfo.jsx
+++ client/src/views/LeafsInfo/LeafsInfo.jsx
@@ -5,13 +5,82 @@
  */
 
 import React, { Component } from 'react';
+import LeafGames from './components/LeafGames.jsx';
+import OfficialItem from '../../components/OfficialItem.jsx';
 import styles from './LeafsInfo.css';
 
 export default class LeafsInfo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mostOfficiated: null,
+            mostWins: null,
+            mostLosses: null
+        };
+    }
+
+    componentDidMount() {
+        // TODO Make GET request to find most officiated gameId
+
+        // TODO Make GET request to the official who officiated the most losses
+
+        // TODO Make GET request to find the official who officiated the most wins
+    }
+
     render() {
+        let mostOfficiatedComp = <p>Loading..</p>;
+        let mostWinsComp = <p>Loading..</p>;
+        let mostLossesComp = <p>Loading..</p>;
+
+        if (this.state.mostOfficiated !== null) {
+            mostOfficiatedComp = (
+                <OfficialItem
+                    id={this.state.mostOfficiated.id}
+                    lastName={this.state.mostOfficiated.lastName}
+                    firstName={this.state.mostOfficiated.firstName}
+                    home={this.state.mostOfficiated.home}
+                />
+            );
+        }
+
+        if (this.state.mostWins !== null) {
+            mostWinsComp = (
+                <OfficialItem
+                    id={this.state.mostWins.id}
+                    lastName={this.state.mostWins.lastName}
+                    firstName={this.state.mostWins.firstName}
+                    home={this.state.mostWins.home}
+                />
+            );
+        }
+
+        if (this.state.mostLosses !== null) {
+            mostLossesComp = (
+                <OfficialItem
+                    id={this.state.mostLosses.id}
+                    lastName={this.state.mostLosses.lastName}
+                    firstName={this.state.mostLosses.firstName}
+                    home={this.state.mostLosses.home}
+                />
+            );
+        }
+
         return (
             <div>
                 <h1>Leafs Information</h1>
+
+                <h2>Maple Leafs Games</h2>
+                <LeafGames />
+
+                <h2>Officiated Most Maple Leafs Games</h2>
+                {mostOfficiatedComp}
+
+                <h2>Officiated Most Maple Leafs Losses</h2>
+                {mostLossesComp}
+
+                <h2>Officiated Most Maple Leafs Wins</h2>
+                {mostWinsComp}
             </div>
         );
     }
---
+++ client/src/views/LeafsInfo/components/LeafGames.jsx
@@ -0,0 +1,40 @@
+/**
+ * LeafGames.jsx
+ * Jordan Mathewson - 250868197
+ * CS3319A - Assignment #3
+ */
+
+import React, { Component } from 'react';
+import GameScore from '../../../components/GameScore.jsx';
+import styles from './LeafGames.css';
+
+export default class LeafGames extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            games: []
+        };
+    }
+    componentDidMount() {
+        // TODO Make GET request to find all Leafs games
+    }
+
+    render() {
+        const gameComps = [];
+
+        for (let game = 0; game < this.state.games.length; game++) {
+            gameComps.push(
+                <div>
+                    <GameScore team1={this.state.games[game].team1} team2={this.state.games[game].team2} key={game}/>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                {gameComps}
+            </div>
+        );
+    }
+}

5b55543918d29a7a79669c2057409341534420f3
Jordan <jordansne@gmail.com>
2017-12-01 23:48:32 -0500

    Create GET /api/officials API

--- server/routes/officials.js
+++ server/routes/officials.js
@@ -6,9 +6,29 @@
 
 const express = require('express');
 const officials = express.Router();
+const database = require('../database');
+const logger = require('../logger');
 
+/**
+ * GET request for retrieving a list of officials ordered by last name.
+ */
 officials.get('/', (req, res) => {
-    res.send('Officials end point');
+    database.query('SELECT * FROM Official ORDER BY lname').then((result) => {
+        // Rename attributes and only send rows
+        const payload = result.rows.map((row) => {
+            return {
+                id: row.officialid,
+                firstName: row.fname,
+                lastName: row.lname,
+                home: row.homecity
+            };
+        });
+
+        res.json({ officials: payload });
+    }).catch((err) => {
+        logger.log('error', 'Error accessing database', { error: err });
+        res.status(500).json({ error: 'Internal Error' });
+    });
 });
 
 module.exports = officials;

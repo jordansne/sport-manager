556417e26a01de24eec97302934a1a6ee5e3e9d6
Jordan <jordansne@gmail.com>
2017-12-01 21:01:02 -0500

    Implement database module for making queries

---
+++ server/database.js
@@ -0,0 +1,35 @@
+/**
+ * database.js
+ * Jordan Mathewson - 250868197
+ * CS3319A - Assignment #3
+ */
+
+const logger = require('./logger');
+const { Pool } = require('pg');
+const pool = new Pool();
+
+module.exports = {
+    // Promise returning function for making SQL queries
+    query: (queryString, params) => {
+        logger.log('info', 'Connecting to postgres server..');
+
+        // Connect to the database
+        return pool.connect().then((client) => {
+            logger.log('info', 'Connected, performing query', { query: queryString });
+
+            // Perform the query
+            return client.query(queryString, params).then((result) => {
+                logger.log('info', 'Query success');
+
+                client.release();
+                return result;
+            // Catch any errors that could occur with making the query
+            }).catch((err) => {
+                logger.log('error', 'Failed to make query', { error: err });
+
+                client.release();
+                throw err;
+            });
+        });
+    }
+};

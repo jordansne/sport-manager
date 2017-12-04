376002dab50503b1f703002283278b636ce7f0f6
Jordan <jordansne@gmail.com>
2017-12-04 03:33:31 -0500

    Get URL_PREFIX from environ, var when building prod files

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,14 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+// For access to environment variables
+require('dotenv').config();
+
 module.exports = merge(common, {
     plugins: [
         new UglifyJSPlugin(),
         new webpack.DefinePlugin({
-            URL_PREFIX: JSON.stringify('/jmathe53/my_app')
+            URL_PREFIX: JSON.stringify(process.env.URL_PREFIX)
         })
     ]
 });
